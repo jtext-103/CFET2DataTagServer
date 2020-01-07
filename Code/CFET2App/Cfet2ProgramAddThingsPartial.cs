8c0738b22ba5101c629b9cfd31c261146755cc07
wangyuxing <wyxchina@foxmail.com>
2020-01-07 16:09:48 +0800

    更新Core和App版本；下一步需要去除EPICS依赖

--- Code/CFET2App/Cfet2ProgramAddThingsPartial.cs
+++ Code/CFET2App/Cfet2ProgramAddThingsPartial.cs
@@ -6,12 +6,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ViewCopy;
-using JTextDAQDataFileOperator.HDF5;
+using Jtext103.CFET2.Core.BasicThings;
+using Nancy.Conventions;
+using Nancy;
+using Jtext103.CFET2.Core.Middleware.Basic;
+using Jtext103.CFET2.CFET2App.DynamicLoad;
 using DataServer;
 using TagManager;
 using TagServer;
 using ShotNoWatcher;
+using JTextDAQDataFileOperator.HDF5;
 
 namespace Jtext103.CFET2.CFET2App
 {
@@ -20,32 +24,36 @@ namespace Jtext103.CFET2.CFET2App
         private void AddThings()
         {
             //If you don't want dynamic load things, please comment out the line below
-            //var loader = new DynamicThingsLoader(this);
+            var loader = new DynamicThingsLoader(this);
 
-            //you can add Thing by coding here
+            //------------------------------Pipeline------------------------------//
+            MyHub.Pipeline.AddMiddleware(new ResourceInfoMidware());
+            MyHub.Pipeline.AddMiddleware(new NavigationMidware());
 
-            //nancy HTTP
+            //------------------------------Nancy HTTP通信模块------------------------------//
             var nancyCM = new NancyCommunicationModule(new Uri("http://localhost:8002"));
             MyHub.TryAddCommunicationModule(nancyCM);
 
-            //拷贝视图文件夹
-            //var myViewsCopyer = new ViewCopyer();
-            //myViewsCopyer.StartCopy();
-            //var myContentCopyer = new ViewCopyer(null, "Content");
-            //myContentCopyer.StartCopy();
+            //you can add Thing by coding here
 
-            var dataServer = new DataServerThing(@"AllConfig.json");
+            //------------------------------Custom View------------------------------//
+            var customView = new CustomViewThing();
+            MyHub.TryAddThing(customView, "/", "customView", "./CustomView");
+
+            //you can add Thing by coding here
+
+            var dataServer = new DataServerThing(@"./ConfigFile/BasePath.json");
             dataServer.dataFileFactoty = new HDF5DataFileFactory();
             MyHub.TryAddThing(dataServer, "/", "dataServer");
 
             var tagManager = new TagManagerThing();
-            MyHub.TryAddThing(tagManager, "/", "tag", @"AllConfig.json");
+            MyHub.TryAddThing(tagManager, "/", "tag", @"./ConfigFile/Database.json");
 
             var tagServer = new TagServerThing();
             MyHub.TryAddThing(tagServer, "/", "tagServer");
 
             var shotWatcher = new ShotNoWatcherThing();
-            MyHub.TryAddThing(shotWatcher, "/", "shotWatcher", @"AShotWatcherConfig.json");
+            MyHub.TryAddThing(shotWatcher, "/", "shotWatcher", @"./ConfigFile/ShotNoWatcher.json");
         }
     }
 }
